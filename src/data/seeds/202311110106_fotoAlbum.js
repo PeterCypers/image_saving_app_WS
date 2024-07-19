4ea703c61b625efd3ebd301a30a17226b0d310eb
Peter Cypers <peter.cypers@student.hogent.be>
2024-07-19 14:18:28 +0200

    seeding fixed, disabled koa helmet security headers else cannot use static file serving

--- src/data/seeds/202311110106_fotoAlbum.js
+++ src/data/seeds/202311110106_fotoAlbum.js
@@ -4,13 +4,14 @@ module.exports = {
     seed: async (knex) => {
         // await knex(tables.fotoalbum).delete();
         // (albumName, creationDate, userID)  
+
         await knex(tables.fotoalbum).insert([
-            { albumID: 1, albumName: 'album one', creationDate: '2023-10-21 00:00:00', userID: 2 },
-            { albumID: 2, albumName: 'album two', creationDate: '2023-10-21 00:00:00', userID: 2 },
-            { albumID: 3, albumName: 'album three', creationDate: '2023-10-22 00:00:00', userID: 2 },
-            { albumID: 4, albumName: 'album four', creationDate: '2023-11-11 00:00:00', userID: 2 },
-            { albumID: 5, albumName: 'album five', creationDate: '2023-11-15 00:00:00', userID: 2 },
-            { albumID: 6, albumName: 'album six', creationDate: '2023-11-15 00:00:00', userID: 2 },
+            { albumID: 1, albumName: 'Vacation 2023', creationDate: '2023-10-21 00:00:00', userID: 2 },
+            { albumID: 2, albumName: 'Nature Trips', creationDate: '2023-10-21 00:00:00', userID: 2 },
+            { albumID: 3, albumName: 'Landscapes', creationDate: '2023-10-22 00:00:00', userID: 2 },
+            { albumID: 4, albumName: 'Work Events', creationDate: '2023-11-11 00:00:00', userID: 2 },
+            { albumID: 5, albumName: 'Family', creationDate: '2023-11-15 00:00:00', userID: 2 },
+            { albumID: 6, albumName: 'Wilderness', creationDate: '2023-11-15 00:00:00', userID: 2 },
         ]);
     },
 };
