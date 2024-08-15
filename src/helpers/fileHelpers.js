9ba5b2f1b99b33eabc9774c50a1045b59e497cc4
Peter Cypers <peter.cypers@student.hogent.be>
2024-08-15 16:32:48 +0200

    resetting main to this working commit

--- src/helpers/fileHelpers.js
+++ src/helpers/fileHelpers.js
@@ -31,7 +31,14 @@ const saveFileToSystem = (fotoFile, userID, baseDir) => {
     const targetPath = path.join(userDirectory, originalName);
 
     // Move the file to the target path
-    fs.renameSync(tempFilePath, targetPath);
+    // fs.renameSync(tempFilePath, targetPath); //-> zorgt voor grove error bij online versie
+
+    // Copy the file to the target path
+    fs.copyFileSync(tempFilePath, targetPath);
+    // Delete the original file
+    fs.unlinkSync(tempFilePath);
+   
+   
 
     // getting the difference between 2 paths:(ter info)
     // const relativePath = path.relative(path.join(__dirname, '..', '..', 'public'), targetPath);
